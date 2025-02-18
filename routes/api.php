623f0c730b5a777b8336a45671d3823b9ef96ec1
WeimperGergo <weimper.geri@gmail.com>
2025-02-18 15:48:37 +0100

    api merge

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\BookOfferController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookOfferController;
 use App\Http\Middleware\Admin;
 use App\Models\BookOffer;
 
