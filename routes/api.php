9e59055d00269430f55e24a939c365d2141b50bf
WeimperGergo <weimper.geri@gmail.com>
2025-02-18 14:31:29 +0100

    api php -> par route hozzaadva
    meg nincs lekerdezes hozzajuk

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,28 @@
 <?php
 
+use App\Http\Controllers\BookOfferController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\Admin;
+use App\Models\BookOffer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
 });
 
+Route::middleware(['auth:sanctum', Admin::class])->group(function () {
+    Route::get('/users', [UserController::class, 'getUsers']);
+});
 
+Route::get('/users', [UserController::class, 'getUsers']);
+Route::get('/user/{id}', [UserController::class, 'getUser']);
+
+Route::get('/user/{id}/mu', [BookOfferController::class, 'getBooksForUsers']);
+Route::get('/user-offers/{id}', [UserController::class, 'getUsersBookOffers']); //Given user's book offers
+
+Route::get('/book-requests', [UserController::class, 'requestedBooks']); //Books requested by users
+Route::get('/user-bookinfo/{id}', [UserController::class, 'getBookInfo']); //Given book's info
+
+Route::get('/books', [BookOfferController::class, 'getBooks']);
