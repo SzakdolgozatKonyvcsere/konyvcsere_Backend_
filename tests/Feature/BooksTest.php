3b2ec347ae88faf0bab19288fa5f5af2d01226ac
WeimperGergo <weimper.geri@gmail.com>
2025-02-25 11:52:19 +0100

    books api

--- tests/Feature/BooksTest.php
+++ tests/Feature/BooksTest.php
@@ -10,13 +10,14 @@ class BooksTest extends TestCase
 {
     public function test_all_offered_books(): void
     {
-        $response = $this->get('/api/books');
+        $response = $this->get('/api/book-offers');
 
         $response->assertStatus(200);
     }
 
     public function test_book_offers_by_user(): void
     {
+        
         $response = $this->get('/api/book-offers/1');
         
         $response->assertStatus(200);
