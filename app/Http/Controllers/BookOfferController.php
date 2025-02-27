54cf74d8d3a3ec240ade4041126745f29060ecab
Csorba Eszter <csorba.eszter.26@gmail.com>
2025-02-28 00:00:14 +0100

    butasag javitva

--- app/Http/Controllers/BookOfferController.php
+++ app/Http/Controllers/BookOfferController.php
@@ -98,10 +98,10 @@ if (!$work) {
 
     public function mostExchangedGenre()
     {
-        $mostExchangedGenre = DB::table('ExchangeHistory as e')
-        ->join('BookOffer as bo', 'e.disered_idem', '=', 'bo.offer_id')
-        ->join('Work as wo', 'bo.wo', '=', 'wo.work_id') 
-        ->join('Genre as ge', 'wo.ge', '=', 'ge.genre_id') 
+        $mostExchangedGenre = DB::table('exchange_histories as e')
+        ->join('book_offers as bo', 'e.desired_item', '=', 'bo.offer_id')
+        ->join('works as wo', 'bo.wo', '=', 'wo.work_id') 
+        ->join('genres as ge', 'wo.ge', '=', 'ge.genre_id') 
         ->where('e.exchange_status', 'a') 
         ->select('ge.genre_name', DB::raw('COUNT(e.exchange_id) as exchange_count')) 
         ->groupBy('ge.genre_name') 
