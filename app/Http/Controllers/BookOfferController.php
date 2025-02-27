8820f14bf68b5fe8cd61cc8d44608e20b40c500d
Csorba Eszter <csorba.eszter.26@gmail.com>
2025-02-27 16:04:51 +0100

    sync

--- app/Http/Controllers/BookOfferController.php
+++ app/Http/Controllers/BookOfferController.php
@@ -23,12 +23,21 @@ class BookOfferController extends Controller
             'genre_id' => 'required|exists:genres,genre_id',
             'title' => 'required|string|max:255'
         ]);
-
+        /*
         $work = Work::firstOrCreate([ //--létezik e már
         //$work = Work::create([
             ['title' => $request->title], // Adj neki egy címet vagy más adatokat
             ['genre_id' => $request->genre_id],
         ]);
+        dd($work);*/
+        $work = Work::where('title', $request->title)->first();
+
+if (!$work) {
+    $work = Work::create([
+        'title' => $request->title,
+        'genre_id' => $request->genre_id,
+    ]);
+}
 
         $book = BookOffer::create([
             'user' => $request->user,
