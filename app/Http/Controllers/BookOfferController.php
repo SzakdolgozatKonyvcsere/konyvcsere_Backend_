08d6168641f127463f4c5945381a4b686f568410
Csorba Eszter <csorba.eszter.26@gmail.com>
2025-03-27 14:37:43 +0100

    ush

--- app/Http/Controllers/BookOfferController.php
+++ app/Http/Controllers/BookOfferController.php
@@ -92,17 +92,24 @@ class BookOfferController extends Controller
         return response()->json($books);
     }
 
-public function getAllBookOffersAvailable() {
+public function getAllBookOffersAvailable(Request $request) {
+    $userId = $request->user()->id;  // Az aktuális felhasználó ID-ja
     $books = DB::table('book_offers')
         ->join('works', 'book_offers.work', '=', 'works.work_id') 
         ->leftJoin('publishers', 'book_offers.publisher', '=', 'publishers.publisher_id') 
         ->leftJoin('written_bies', 'works.work_id', '=', 'written_bies.work')
         ->leftJoin('authors', 'written_bies.author', '=', 'authors.author_id')
-        ->where('book_offers.book_status', '=', 's', 'or', 'book_offers.book_status', '=', 'f') 
-        ->select('book_offers.offer_id', 'works.title', 'publishers.publisher_name', 'book_offers.book_status', 
+        ->leftJoin('genres', 'works.genre_id', '=', 'genres.genre_id')
+        ->leftJoin('users', 'book_offers.user', '=', 'users.id')
+        ->where(function ($query) {
+            $query->where('book_offers.book_status', '=', 's')
+                  ->orWhere('book_offers.book_status', '=', 'f');
+        }) 
+        ->where('book_offers.user', '!=', $userId)  // Csak azok a könyvek, amiket nem ő töltött fel
+        ->select('users.id', 'book_offers.offer_id', 'works.title', 'publishers.publisher_name', 'book_offers.book_status', 
         DB::raw('GROUP_CONCAT(authors.author_name SEPARATOR ", ") as authors'), 
-        'book_offers.publication_year', 'book_offers.language', 'book_offers.quality', 'book_offers.user') 
-        ->groupBy('book_offers.offer_id','works.title', 'publishers.publisher_name', 'book_offers.book_status', 'book_offers.publication_year', 'book_offers.language', 'book_offers.quality', 'book_offers.user')
+        'book_offers.publication_year', 'book_offers.language', 'book_offers.quality', 'book_offers.user', 'genres.genre_name') 
+        ->groupBy('users.id', 'book_offers.offer_id','works.title', 'publishers.publisher_name', 'book_offers.book_status', 'book_offers.publication_year', 'book_offers.language', 'book_offers.quality', 'book_offers.user')
         ->get();
 
     return response()->json($books); 
--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -25,7 +25,7 @@ class WorkController extends Controller
             'language' => 'required|string|max:255',
             'publication_year' => 'required|integer',
             'quality' => 'required|integer',
-            'img_url' => ['nullable', 'mimes:jpg,png,gif,jpeg,svg', 'max:2048'],
+            //'img_url' => ['nullable', 'mimes:jpg,png,gif,jpeg,svg', 'max:2048'],
         ]);
 
         // Genre validálása és keresése
@@ -83,13 +83,13 @@ class WorkController extends Controller
             'publication_year' => $request->publication_year,
             'quality' => $request->quality,
             'book_status' => 's', // Szabad státusz alapértelmezetten
-            'img_url' => $imagePath,
+            //'img_url' => $imagePath,
         ]);
 
         // Válasz visszaadása
         return response()->json([
             'book' => $book,
-            'img_url' => $imagePath,  // Visszaadjuk az új képet
+            //'img_url' => $imagePath,  // Visszaadjuk az új képet
         ]);
     }
 }
